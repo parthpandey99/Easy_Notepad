61b1fef95b07acfae75d4db465e27c5f60c85e68
parthpandey99 <prarabdhpandey51@gmail.com>
2025-08-04 01:32:10 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/example/easy_notepad/MainActivity.java
+++ android/app/src/main/java/com/example/easy_notepad/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.easy_notepad;
 
-import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 }
